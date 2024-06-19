55f0d5450f4199eef0d443a1809adfd99e465537
namirahrasul <n.rsl.136@gmail.com>
2024-06-19 19:34:23 +0600

    mutils.py annotated

--- tools/mutils.py
+++ tools/mutils.py
@@ -3,14 +3,15 @@ import os
 import shutil
 import time
 
-
+#counts number of trainable parameters
+#same as net_utils but different print
 def count_parameters(model):
     number_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print('Total Parameters: {:d} or {:.2f}M'.format(number_params, number_params / (1024 * 1024)))
+    print('Total Parameters: {:d} or {:.2f}M'.format(number_params, number_params / (1024 * 1024))) #in millions
     return number_params
 
 
-def contains(key, lst):
+def contains(key, lst): #return true if lst contains the key
     flag = False
     for item in lst:
         if key == item:
@@ -18,19 +19,19 @@ def contains(key, lst):
     return flag
 
 
-def make_empty_dir(new_dir):
+def make_empty_dir(new_dir): #deletes previous directory containing files and makes new empty one
     if os.path.exists(new_dir):
         shutil.rmtree(new_dir)
     os.makedirs(new_dir, exist_ok=True)
 
 
 def get_timestamp():
-    return str(time.time()).replace('.', '')
+    return str(time.time()).replace('.', '') #returns time as secondsmilliseconds vs seconds.milliseconds can be used timestamp identifier
 
 
-def get_formatted_time():
+def get_formatted_time(): #returns current time formatted as YYYYMMDD-HHMMSS
     return datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #if the script is being run directly (as opposed to being imported as a module).
     pass
--- util/schedulers.py
+++ util/schedulers.py
@@ -1,16 +1,24 @@
 from torch.optim.lr_scheduler import _LRScheduler
+#Learning rate scheduler
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-
+#Reduce learning rate when a metric has stopped improving. Models often benefit from reducing the learning rate by a factor of 2-10 once learning stagnates
 
 class GradualWarmupScheduler(_LRScheduler):
-    """ Gradually warm-up(increasing) learning rate in optimizer.
+    #Starts with a small learning rate and gradually increases it over a few initial epochs or iterations. This is particularly useful in preventing the model from diverging in the initial phase of training and is often used in training deep networks from scratch.
+
+    #Pros: Prevents early divergence; stabilizes training.
+    #Cons: Requires tuning of warm-up duration and rate limits.
+    #Use Case: Crucial for training deep networks from scratch.
+    """(mingcv) Gradually warm-up(increasing) learning rate in optimizer.
     Proposed in 'Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour'.
     Args:
         optimizer (Optimizer): Wrapped optimizer.
         multiplier: target learning rate = base lr * multiplier if multiplier > 1.0. if multiplier = 1.0, lr starts from 0 and ends up with the base_lr.
         total_epoch: target learning rate is reached at total_epoch, gradually
         after_scheduler: after target_epoch, use this scheduler(eg. ReduceLROnPlateau)
-    """
+    """ 
+    #total_epoch (number of epochs to reach the target learning rate
+    #after scheduler: scheduler after warmup
 
     def __init__(self, optimizer, multiplier, total_epoch, after_scheduler=None):
         self.multiplier = multiplier
@@ -18,28 +26,30 @@ class GradualWarmupScheduler(_LRScheduler):
             raise ValueError('multiplier should be greater thant or equal to 1.')
         self.total_epoch = total_epoch
         self.after_scheduler = after_scheduler
-        self.finished = False
+        self.finished = False #initializes a flag to indicate if warm-up is finished.
         super(GradualWarmupScheduler, self).__init__(optimizer)
 
-    def get_lr(self):
-        if self.last_epoch > self.total_epoch:
-            if self.after_scheduler:
-                if not self.finished:
-                    self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
-                    self.finished = True
+    def get_lr(self): #get the current learning rates.
+        if self.last_epoch > self.total_epoch: #warm is ovcer , target lr reached
+            if self.after_scheduler: #plateuLR used
+                if not self.finished: #sets the base learning rates for the after_scheduler to be multiplier times the base learning rates and returns rlearningrate of after scheduler.
+                    self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs] #self.base_lrs intialized from _LRScheduler
+                    self.finished = True #warm up done
                 return self.after_scheduler.get_lr()
             return [base_lr * self.multiplier for base_lr in self.base_lrs]
 
-        if self.multiplier == 1.0:
+        if self.multiplier == 1.0: #increase lienarly and propertionally to number of epochs run
             return [base_lr * (float(self.last_epoch) / self.total_epoch) for base_lr in self.base_lrs]
-        else:
-            return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in
-                    self.base_lrs]
-
+        else: #increaase lr non linearly
+            return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in   
+                    self.base_lrs]       
+        "??? why this formula for non linear"
+    
     def step_ReduceLROnPlateau(self, metrics, epoch=None):
-        if epoch is None:
+        #metrics: A value that will be monitored (e.g., validation loss) to decide if the learning rate needs adjustment.
+        if epoch is None: #not resuming from a specific epoch
             epoch = self.last_epoch + 1
-        self.last_epoch = epoch if epoch != 0 else 1  # ReduceLROnPlateau is called at the end of epoch, whereas others are called at beginning
+        self.last_epoch = epoch if epoch != 0 else 1  # (mingcv) ReduceLROnPlateau is called at the end of epoch, whereas others are called at beginning
         if self.last_epoch <= self.total_epoch:
             warmup_lr = [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in
                          self.base_lrs]
