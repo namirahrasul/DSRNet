186903c47ff2f5fdc430f066f5cbcc877188a3c6
namirahrasul <n.rsl.136@gmail.com>
2024-06-19 16:09:54 +0600

    annotated util.py

--- util/util.py
+++ util/util.py
@@ -10,7 +10,7 @@ import torch
 import torch.nn as nn
 import yaml #Imports PyYAML for parsing YAML files.
 from PIL import Image #Imports the Python Imaging Library for image processing.
-from skimage.metrics import peak_signal_noise_ratio as compare_psnr #Imports the PSNR metric from skimage  to quantify reconstruction quality for images and video subject to lossy compression.
+from skimage.metrics import peak_signal_noise_ratio as compare_psnr #Imports the PSNR metric from skimage  to quantify reconstruction quality for images and video subject to lossy compression. Higher is better
 from skimage.metrics import structural_similarity #Imports the SSIM metric from skimage.A full-reference image quality evaluation index that measures image similarity from three aspects: brightness, contrast, and structure.
 
 
@@ -159,9 +159,10 @@ def mkdir(path):
 def set_opt_param(optimizer, key, value):
     for group in optimizer.param_groups:
         group[key] = value
+'???'
 
 
-def vis(x):
+def vis(x):#display an image in image viewer of system regardless if tensor or numpy array
     if isinstance(x, torch.Tensor):
         Image.fromarray(tensor2im(x)).show()
     elif isinstance(x, np.ndarray):
@@ -171,27 +172,32 @@ def vis(x):
 
 
 """tensorboard"""
-from tensorboardX import SummaryWriter
+from tensorboardX import SummaryWriter #for data logging
 from datetime import datetime
 
 
-def get_summary_writer(log_dir):
+def get_summary_writer(log_dir): #log_dir:directy to store logs
     if not os.path.exists(log_dir):
         os.mkdir(log_dir)
-    log_dir = os.path.join(log_dir, datetime.now().strftime('%b%d_%H-%M-%S') + '_' + socket.gethostname())
+    log_dir = os.path.join(log_dir, datetime.now().strftime('%b%d_%H-%M-%S') + '_' + socket.gethostname()) 
     if not os.path.exists(log_dir):
         os.mkdir(log_dir)
-    writer = SummaryWriter(log_dir)
+    writer = SummaryWriter(log_dir) #each time you run your experiments, logs are saved in a new directory with current time, date and machine hostname
     return writer
 
 
-class AverageMeters(object):
-    def __init__(self, dic=None, total_num=None):
-        self.dic = dic or {}
-        # self.total_num = total_num
-        self.total_num = total_num or {}
 
-    def update(self, new_dic):
+# keeps track of running avergae of metrics
+class AverageMeters(object): #all classes inherit from object
+    def __init__(self, dic=None, total_num=None):
+        #dic: dictionary of metrics
+        #total_num:track of the counts of each metric)
+        self.dic = dic or {}
+        #(mingcv) self.total_num = total_num
+        self.total_num = total_num or {}
+        
+
+    def update(self, new_dic): # method appends self.dic and updates total_num with new values.
         for key in new_dic:
             if not key in self.dic:
                 self.dic[key] = new_dic[key]
@@ -199,42 +205,60 @@ class AverageMeters(object):
             else:
                 self.dic[key] += new_dic[key]
                 self.total_num[key] += 1
-        # self.total_num += 1
+        # (mingcv)self.total_num += 1
 
-    def __getitem__(self, key):
+    def __getitem__(self, key): #overriding [], returns average value for a metric
         return self.dic[key] / self.total_num[key]
 
-    def __str__(self):
+    def __str__(self):  #overrider str() and print() to show key and values formatted
         keys = sorted(self.keys())
         res = ''
         for key in keys:
             res += (key + ': %.4f' % self[key] + ' | ')
         return res
 
-    def keys(self):
+    def keys(self): #returns all the metric names
         return self.dic.keys()
 
-
-def write_loss(writer, prefix, avg_meters, iteration):
+'''???why _loss'''
+def write_loss(writer, prefix, avg_meters, iteration): 
+    #writer is the writer object for logging,SummaryWriter instance
+    #prefix is keyword added before each metric e.g. "train"  it logs "accuracy": 85.4 as "train/accuracy"
+    #avg_meters: A dictionary argument where keys are metric names (like ‘loss’, ‘accuracy’) and values are their corresponding values at the current iteration (e.g., 0.23, 85.4).
+    #iteration: An integer argument representing the current iteration or step number in the training or evaluation process.
     for key in avg_meters.keys():
-        meter = avg_meters[key]
-        writer.add_scalar(
-            os.path.join(prefix, key), meter, iteration)
+            meter = avg_meters[key] # for each key, we get the corresponding value from the avg_meters dictionary and assign it to the variable meter.
+            writer.add_scalar(os.path.join(prefix, key), meter, iteration)
+    # writer.add_scalar(): This method call logs the scalar value (metric) to the writer
+'''
+avg_meters = {
+    “loss”: 0.23,
+    “accuracy”: 85.4,
+    “precision”: 88.1
+}
+write_loss(writer, “train”, avg_meters, 10)
 
+Logging: train/loss = 0.23 at step 10
+Logging: train/accuracy = 85.4 at step 10
+Logging: train/precision = 88.1 at step 10
 
-"""progress bar"""
+'''
+"""(Mingcv)progress bar"""
 import socket
 
-# _, term_width = os.popen('stty size', 'r').read().split()
-term_width = 136
-
-TOTAL_BAR_LENGTH = 65.
-last_time = time.time()
-begin_time = last_time
+# _, term_width = os.popen('stty size', 'r').read().split() 
+term_width = 136 #hardcoded terminal width to 136 characters instead of dymeically reading using stty siz
+ 
+TOTAL_BAR_LENGTH = 65. #length of the progress bar.
+last_time = time.time() #when progress is finished
+begin_time = last_time  # used to calculate the elapsed time for each step and the total process.
 
 
 def progress_bar(current, total, msg=None):
-    global last_time, begin_time
+                                        #current: The current progress count.
+                                        #total: The total count that represents 100% progress.
+                                        #msg: An optional message to display alongside the progress bar.
+    global last_time, begin_time # for persistence in step_time and total_time
     if current == 0:
         begin_time = time.time()  # Reset for new bar.
 
@@ -265,7 +289,7 @@ def progress_bar(current, total, msg=None):
     for i in range(term_width - int(TOTAL_BAR_LENGTH) - len(msg) - 3):
         sys.stdout.write(' ')
 
-    # Go back to the center of the bar.
+    #(mingcv) Go back to the center of the bar.
     for i in range(term_width - int(TOTAL_BAR_LENGTH / 2) + 2):
         sys.stdout.write('\b')
     sys.stdout.write(' %d/%d ' % (current + 1, total))
@@ -277,7 +301,7 @@ def progress_bar(current, total, msg=None):
     sys.stdout.flush()
 
 
-def format_time(seconds):
+def format_time(seconds): #convert seconds to days,hourse,mintures,seconds,milliseconds
     days = int(seconds / 3600 / 24)
     seconds = seconds - days * 3600 * 24
     hours = int(seconds / 3600)
@@ -310,8 +334,8 @@ def format_time(seconds):
     return f
 
 
-def parse_args(args):
-    str_args = args.split(',')
+def parse_args(args): #converts numeric args seperated by commas into a [] of ints
+    str_args = args.split(',') #str1,atr2,str3 becomes [a,b,c]
     parsed_args = []
     for str_arg in str_args:
         arg = int(str_arg)
@@ -319,66 +343,93 @@ def parse_args(args):
             parsed_args.append(arg)
     return parsed_args
 
+#In order to overcome vanishing/explosding gradient, Xavier initialization was introduced. It tries to keep variance of all the layers equal but assumes linear actiivation
+# Kaiming He initialization, which takes activation function into account. 
 
-def weights_init_kaiming(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+def weights_init_kaiming(m): #layer or module in NN
+    classname = m.__class__.__name__ # gets the class name of the layer m using its __class__ attribute and __name__ property. 
+    if classname.find('Conv') != -1: #returns -1 if the substring is not found, So here if found
         nn.init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
+
+    #nn.init.kaiming_normal:Fill the input Tensor(m.weight.data) with values using a Kaiming normal distribution.The method is described in Delving deep into rectifiers: Surpassing human-level performance on ImageNet classification - He, K. et al. (2015). The resulting tensor will have values sampled from N(0,std^2) where std=gain/sqrt(fan_mode)
+    #a (float) – the negative slope of the rectifier used after this layer only used with Leaky RelU (0-not used)
+    #mode: fan_in(default) or fan_out.Choosing 'fan_in' preserves the magnitude of the variance of the weights in the forward pass. 
+    #here intialising for relu,preserving variance of weights in forward pass
     elif classname.find('Linear') != -1:
         nn.init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
+        # here intialising for relu,preserving variance of weights in forward pass
     elif classname.find('BatchNorm') != -1:
         # nn.init.uniform(m.weight.data, 1.0, 0.02)
         m.weight.data.normal_(mean=0, std=math.sqrt(2. / 9. / 64.)).clamp_(-0.025, 0.025)
         nn.init.constant(m.bias.data, 0.0)
+        "??? why std=math.sqrt(2. / 9. / 64.)"
+        # .clamp_(-0.025, 0.025): Clamps (limits) the values in the tensor to be between -0.025 and 0.025.
+        #bias of the batch normalization layer intiliazed to a constant value of 0.0.
 
 
-def batch_PSNR(img, imclean, data_range):
-    Img = img.data.cpu().numpy().astype(np.float32)
-    Iclean = imclean.data.cpu().numpy().astype(np.float32)
+def batch_PSNR(img, imclean, data_range): #calculates avg PSNR for 1 batch of images
+    #img batch of images to be evaluated with noise
+    #imclean: batch of ground truth images no noise
+    Img = img.data.cpu().numpy().astype(np.float32) #converts img to float numpy array in cpu
+    #- img.data: Detaches the data from the computation graph.
+    #cpu(): Transfers the tensor from GPU to CPU if it’s on GPU.
+    #numpy(): Converts the tensor to a NumPy array.
+    #astype(np.float32): Converts the array type to float32.
+    Iclean = imclean.data.cpu().numpy().astype(np.float32) #same for ground truth
     PSNR = 0
-    for i in range(Img.shape[0]):
-        PSNR += compare_psnr(Iclean[i, :, :, :], Img[i, :, :, :], data_range=data_range)
-    return PSNR / Img.shape[0]
+    for i in range(Img.shape[0]): #for all batches add the skmetric.psnr()
+        PSNR += compare_psnr(Iclean[i, :, :, :], Img[i, :, :, :], data_range=data_range) 
+        #data range: maximum - inimum possible values
+    return PSNR / Img.shape[0] #average psnr per batch
 
-
+#calculates avg SSIM for 1 batch of images
 def batch_SSIM(img, imclean):
     Img = img.data.cpu().permute(0, 2, 3, 1).numpy().astype(np.float32)
+    #converts img to float numpy array in cpu
+    #img.data: Detaches the data from the computation graph.
+    #cpu(): Transfers the tensor from GPU to CPU if it’s on GPU.
+    #numpy(): Converts the tensor to a NumPy array.
+    #astype(np.float32): Converts the array type to float32.
     Iclean = imclean.data.cpu().permute(0, 2, 3, 1).numpy().astype(np.float32)
+    #permute(0, 2, 3, 1): Changes the order of dimensions from (batch_size, channels, height, width) to (batch_size, height, width, channels). 
     SSIM = 0
 
-    for i in range(Img.shape[0]):
+    for i in range(Img.shape[0]): #for all batches add the skmetric.ssim()
         SSIM += structural_similarity(Iclean[i, :, :, :], Img[i, :, :, :], win_size=11,
                                       multichannel=True, data_range=1)
-    return SSIM / Img.shape[0]
+        #win_size:The side-length of the sliding window used in comparison. Must be an odd value
+        #multichannel deprecated ...equivalent to channel-axis 
+        "??? multichannel=True equal to what number in channel-axis"
+    return SSIM / Img.shape[0] #average ssim for1 batch
 
 
-def data_augmentation(image, mode):
+def data_augmentation(image, mode): #augment given image based on mode passed by manipulating numpy array
     out = np.transpose(image, (1, 2, 0))
     if mode == 0:
-        # original
+        #(mingcv)original
         out = out
     elif mode == 1:
-        # flip up and down
+        #(mingcv)flip up and down
         out = np.flipud(out)
     elif mode == 2:
-        # rotate counterwise 90 degree
+        #(mingcv)rotate counterwise 90 degree
         out = np.rot90(out)
     elif mode == 3:
-        # rotate 90 degree and flip up and down
+        #(mingcv)rotate 90 degree and flip up and down
         out = np.rot90(out)
         out = np.flipud(out)
     elif mode == 4:
-        # rotate 180 degree
+        #(mingcv)rotate 180 degree
         out = np.rot90(out, k=2)
     elif mode == 5:
-        # rotate 180 degree and flip
+        #(mingcv)rotate 180 degree and flip
         out = np.rot90(out, k=2)
         out = np.flipud(out)
     elif mode == 6:
-        # rotate 270 degree
+        #(mingcv)rotate 270 degree
         out = np.rot90(out, k=3)
     elif mode == 7:
-        # rotate 270 degree and flip
+        #(mingcv)rotate 270 degree and flip
         out = np.rot90(out, k=3)
         out = np.flipud(out)
     return np.transpose(out, (2, 0, 1))
