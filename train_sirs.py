2f78ffd9a38c93584fae308e3c8847781bfc9781
namirahrasul <n.rsl.136@gmail.com>
2024-06-27 19:06:49 +0600

    cuda out of memory if more than 8 threads on p100

--- train_sirs.py
+++ train_sirs.py
@@ -48,7 +48,7 @@ train_dataloader_VOC = datasets.DataLoader(
     num_workers=opt.nThreads, pin_memory=True)
 train_dataloader_real = datasets.DataLoader(
     train_dataset_real, batch_size=20, shuffle=False,
-    num_workers=16, pin_memory=True)
+    num_workers=opt.nThreads, pin_memory=True)
 #train_dataloader_nature = datasets.DataLoader(train_dataset_nature, batch_size=opt.batchSize, shuffle=not opt.serial_batches,num_workers=opt.nThreads, pin_memory=True)
 ################################################
 eval_dataset_real = datasets.DSRTestDataset(join(datadir, f'test/real20_{opt.real20_size}'),
