b7a338a77e86323c88112906994e6abee948e28e
namirahrasul <n.rsl.136@gmail.com>
2024-06-27 23:53:57 +0600

    revert load_checkpoint

--- util/__init__.py
+++ util/__init__.py
@@ -4,12 +4,14 @@ from .util import *
 
 def load_checkpoint(model, ckpt_path):
     checkpoint = torch.load(ckpt_path)
-    if 'weight' in checkpoint:
-        checkpoint = checkpoint['weight']
+    if 'model' in checkpoint:
+        checkpoint = checkpoint['model']
+    if 'state_dict' in checkpoint:
+        checkpoint = checkpoint['state_dict']
     ckpt = {}
     for k, v in checkpoint.items():
         if k.startswith('module.'):
             ckpt[k[7:]] = v
         else:
             ckpt[k] = v
-    model.load_state_dict(ckpt) #strict=False by nami
+    model.load_state_dict(ckpt)
