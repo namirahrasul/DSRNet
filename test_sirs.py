3e812b9cd109ff81bb5cb83eedd767929c58e262
namirahrasul <n.rsl.136@gmail.com>
2024-06-28 19:21:41 +0600

    realdataset working with real45

--- test_sirs.py
+++ test_sirs.py
@@ -17,8 +17,9 @@ opt.no_log = True
 opt.display_id = 0
 opt.verbose = False
 
-test_dataset_real = datasets.RealDataset('./data')
-
+datadir = os.path.join(opt.base_dir, 'test/real45')
+test_dataset_real = datasets.RealDataset(datadir)
+#test_dataset_real = datasets.RealDataset('./data')
 test_dataloader_real = datasets.DataLoader(test_dataset_real, batch_size=1, shuffle=True, num_workers=opt.nThreads,
                                            pin_memory=True)
 
